ffc41336c719b37804e745035af6b85203f4fcbc
Craig Pullar <craigmichaelpullar@gmail.com>
2018-10-03 19:02:14 -0400

    minor refactor

--- src/api/polygons.js
+++ src/api/polygons.js
@@ -5,12 +5,12 @@ import polygonModelGen from '../models/polygons';
 export default ({ config, db }) => resource({
   id: 'polygons',
 
-  load: async ({ query: { bounds = '' }}, id, callback) => {
+  load: async ({ query: { bounds = '' }}, polygonQueryType, callback) => {
     const boundsArray = getBoundsArrayFromQuery({ queryBounds: bounds });
-    const queryError = getLoadError({ boundsArray, polygonType: id });
+    const queryError = getLoadError({ boundsArray, polygonType: polygonQueryType });
     //TODO: get polygon list for bounds
     try {
-      const polygons = !queryError ? await polygonModelGen({ config, db }).getPolygonByQueryType({ queryType: id }) : []
+      const polygons = !queryError ? await polygonModelGen({ config, db }).getPolygonByQueryType({ queryType: polygonQueryType }) : []
       callback(queryError, polygons);
     } catch (error) {
       console.error(error);
