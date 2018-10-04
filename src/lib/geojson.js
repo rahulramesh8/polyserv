b908be3a765b909fa60bb2e94f6d7e244be025c3
Craig Pullar <craigmichaelpullar@gmail.com>
2018-10-04 19:58:31 -0400

    refactor and error checking

--- src/lib/geojson.js
+++ src/lib/geojson.js
@@ -1,4 +1,7 @@
-export const mapDataToGeoJson = ({
+import { DB_FIELDS } from '../constants/polygons';
+import { functionWithRequiredParams } from './util';
+
+export const createGeoJson = ({
   type,
   coordinates,
   geometryType,
@@ -15,3 +18,22 @@ export const mapDataToGeoJson = ({
       type: propertyType
     },
 });
+
+const createGeoJsonRequiredParams = functionWithRequiredParams({
+  fn: createGeoJson,
+  requiredParams: [
+    'type',
+    'coordinates',
+    'geometryType',
+    'name',
+    'propertyType'
+  ],
+});
+
+export const mapPolygonRecordToGeoJson = polygon => createGeoJsonRequiredParams({
+    type: polygon[DB_FIELDS.TYPE],
+    coordinates: polygon[DB_FIELDS.GEOMETRY_COORDINATES],
+    geometryType: polygon[DB_FIELDS.GEOMETRY_TYPE],
+    name: polygon[DB_FIELDS.PROPERTIES_NAME],
+    propertyType: polygon[DB_FIELDS.PROPERTIES_TYPE]
+  });
--- src/models/polygons.js
+++ src/models/polygons.js
@@ -1,19 +1,9 @@
 import { polygonTableName } from "../constants/polygons";
-import { mapDataToGeoJson } from "../lib/geojson";
-import { DB_FIELDS } from '../constants/polygons';
-//TODO: decide on a polygon modal
+import { mapPolygonRecordToGeoJson } from "../lib/geojson";
 
 export default ({ config, db }) => ({
   getAllPolygons: async ({ limit = 10 } = {}) => {
     const dbResponse = await db.query(`SELECT * FROM ${polygonTableName} LIMIT ${limit}`);
-    return dbResponse.rows.map( polygon => {
-      return mapDataToGeoJson({
-        type: polygon[DB_FIELDS.TYPE],
-        coordinates: polygon[DB_FIELDS.GEOMETRY_COORDINATES],
-        geometryType: polygon[DB_FIELDS.GEOMETRY_TYPE],
-        name: polygon[DB_FIELDS.PROPERTIES_NAME],
-        propertyType: polygon[DB_FIELDS.PROPERTIES_TYPE]
-      });
-    });
+    return dbResponse.rows.map(mapPolygonRecordToGeoJson);
   }
 });
